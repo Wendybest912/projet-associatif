d655e5e9dfa0b7bf09939f3ae737a60738fd0d3d
Wendybest912 <filofilorial@gmail.com>
2025-07-06 23:14:13 +0200

    Delete projet asso directory

--- projet asso/contact.js
+++
@@ -1,9 +0,0 @@
-// Gestion de l'affichage des champs de don
-document.getElementById('subject').addEventListener('change', function() {
-            const donFields = document.getElementById('don-fields');
-            if (this.value === 'don') {
-                donFields.style.display = 'block';
-            } else {
-                donFields.style.display = 'none';
-            }
-        });
